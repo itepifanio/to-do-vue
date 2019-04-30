d9623bde000d43010bdcad5db1ef08ab1cd0bda0
Ítalo Epifânio <italoepifaniols@gmail.com>
2019-04-30 13:51:33 -0300

    Instala body-parser e cria todo

--- src/api/controllers/todoController.js
+++ src/api/controllers/todoController.js
@@ -7,5 +7,11 @@ const todos = new Todo(dao);
 exports.todoList = function(req, res){
     todos.getAll().then(
         ev => res.json(ev)
-    )
+    );
+}
+
+exports.todoStore = function(req, res){
+    const {title, description, date, kanbanid} = req.body;
+    todos.create(title, description, date, kanbanid);
+    res.status(200).send('Success');
 }
--- src/api/router/express.js
+++ src/api/router/express.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 
 app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(bodyParser());
 
 /* ******************** CONTROLLERS ************************* */
 const todoController = require('../controllers/todoController');
@@ -16,5 +19,6 @@ const kanbanController = require('../controllers/kanbanController');
 app.get('/api/kanbans', kanbanController.kanbanList);
 /* ******************** TODO ************************* */
 app.get('/api/todos', todoController.todoList);
+app.post('/api/todos/store', todoController.todoStore);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
