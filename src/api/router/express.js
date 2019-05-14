6871937a8484e1a26eabbb8579c932dc8d8b19a7
Epitacio <bessa.epitacio@gmail.com>
2019-05-14 16:13:52 -0300

    Adicioanndo cookie e session

--- src/api/router/express.js
+++ src/api/router/express.js
@@ -1,11 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cookieParser = require('cookie-parser')
+var session = require('express-session');
+var user = require('../models/user');
+
+app.use(cookieParser());
+
+app.use(session({
+    key: 'user_id',
+    secret: 'nothing',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 100000
+    }
+}));
 
 app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
+});
+
+/* ******************** LOGIN ************************* */
+var checkLogin = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/home');
+    } else {
+        next();
+    }    
+};
+
+app.get(/api/, checkLogin, (req, res) => {
+	res.redirect('/')
 });
 
 /* ******************** CONTROLLERS ************************* */
