259516b46b2e0fed8569f00ff1bfadab3d1dd7c4
Ítalo Epifânio <italoepifaniols@gmail.com>
2019-05-28 14:36:21 -0300

    Inicia drag and drop

--- src/components/todo/TodoList.vue
+++ src/components/todo/TodoList.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-        <todo v-for="(todo) in todos" :key="todo.id" :todo.sync="todo"></todo>
+        <draggable v-model="todos" group="todos" @start="drag=true" @end="drag=false">
+            <div v-for="todo in todos" :key="todo.id">
+                <todo :todo="todo"></todo>
+            </div>
+            <!-- <div v-for="element in myArray" :key="element.id">{{element.name}}</div> -->
+        </draggable>
+        
     </div>
 </template>
 
 <script type="text/javascript">
 
     import Todo from './Todo';
+    import draggable from 'vuedraggable';
 
     export default {
         props: ['todos'],
         components: {
-            Todo,
+            Todo, draggable
         }
     };
 </script>
